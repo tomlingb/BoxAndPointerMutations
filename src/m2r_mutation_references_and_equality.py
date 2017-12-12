21a7a562b3599a1e15a6f03baa13de323a29010e
tomlingb <tomlingb@rose-hulman.edu>
2017-12-12 01:59:44 -0500

    Finished

--- src/m2r_mutation_references_and_equality.py
+++ src/m2r_mutation_references_and_equality.py
@@ -7,10 +7,11 @@ This module demonstrates:
  5. Two equality operators:   is   ==
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Geoffrey Tomlinson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
+
 
 ########################################################################
 # Students:  You will not write any code in this module,
@@ -103,7 +104,7 @@ def main():
     print(point, point2)  # Prints the two DIFFERENT rg.Points
 
     # ------------------------------------------------------------------
-    # 5. Shows the difference betwee the   is   operator
+    # 5. Shows the difference between the   is   operator
     #      (two things refer to the same place in memory)
     #    and the   ==   operator (two things contain the same data).
     # ------------------------------------------------------------------
